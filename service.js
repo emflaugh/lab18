525609d5c3f994a7e022245e78cf5549b9b99a1e
Elyse <elyse@redcurrent.com>
2017-07-26 14:43:17 -0400

    added output functionality

--- service.js
+++ service.js
@@ -12,14 +12,15 @@ app.factory('weatherFactory', function($http) {
     function setWeather(userLat, userLong) {
       var p = $http({
         method: 'GET',
-        url: 'https://simple-weather.p.mashape.com/weather',
+        url: 'https://simple-weather.p.mashape.com/weatherdata?lat=' + userLat + '&lng=' + userLong + "'",
         headers: {
           'X-Mashape-Key': 'QkihKpadnhmshOMCO0GPfGqmCMTBp17slh5jsnjyJp8KMN99IN',
           'Accept': 'text/plain'
         }
-      }).then(function successfulCallback(result) {
-        console.log(result.status, result.headers, result.body);
-        finalData = result.headers;
+      }).then(function successfulCallback(response) {
+        finalData = response.data.query.results.channel;
+        console.log(response);
+
       });
       return p;
     };
